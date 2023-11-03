2adf771f3cca6fd6f572b6ad71264602ab4d652a
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2023-11-03 23:41:30 +0300

    ♻️ split tests into separate files

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -1,93 +1,16 @@
 import { test, expect } from "@playwright/test";
 import type { GPTResponse, GPTChoice } from "../src/lib/types";
 
-test.describe("App", () => {
+test.describe("App form", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:5173/react-chatgpt");
     await page.getByTestId("lang-switch").click();
     await page.getByRole("menuitem", { name: "English" }).click();
-  });
-
-  test("shows step 1 and doesn't other steps, with interactivity disabled for them", async ({
-    page,
-  }) => {
-    expect(page.getByRole("heading", { name: "Step 1: Provide a Secret" }))
-      .toBeVisible;
-    expect(page.getByRole("heading", { name: "Step 2: Select a Model" })).not
-      .toBeVisible;
-    expect(
-      page.getByRole("heading", { name: "Step 3 (optional): Set Context" })
-    ).not.toBeVisible;
-    expect(page.getByRole("heading", { name: "Step 4: Chat Away!" })).not
-      .toBeVisible;
-
-    // we shouldn't be able to Tab into those
-    expect(page.getByTestId("form-next-2")).toBeDisabled;
-    expect(page.getByTestId("form-next-3")).toBeDisabled;
-    expect(page.getByTestId("form-back-2")).toBeDisabled;
-    expect(page.getByTestId("form-back-3")).toBeDisabled;
-    expect(page.getByTestId("form-back-4")).toBeDisabled;
-    expect(page.getByTestId("form-submit")).toBeDisabled;
-    expect(page.getByTestId("form-model-select")).toBeDisabled;
-    expect(page.getByTestId("form-context-textarea")).toBeDisabled;
-    expect(page.getByTestId("form-prompt-textarea")).toBeDisabled;
-    expect(page.getByTestId("form-temperature-slider")).toBeDisabled;
-  });
-
-  test("shows step 1 and doesn't show steps 1 and 2", async ({ page }) => {
-    await page.getByTestId("form-next-1").click();
-    expect(page.getByRole("heading", { name: "Step 1: Provide a Secret" })).not
-      .toBeVisible;
-    expect(page.getByRole("heading", { name: "Step 2: Select a Model" }))
-      .toBeVisible;
-    expect(
-      page.getByRole("heading", { name: "Step 3 (optional): Set Context" })
-    ).not.toBeVisible;
-    expect(page.getByRole("heading", { name: "Step 4: Chat Away!" })).not
-      .toBeVisible;
-
-    // we shouldn't be able to Tab into those
-    expect(page.getByTestId("form-next-1")).toBeDisabled;
-    expect(page.getByTestId("form-next-2")).not.toBeDisabled;
-    expect(page.getByTestId("form-next-3")).toBeDisabled;
-    expect(page.getByTestId("form-back-2")).toBeDisabled;
-    expect(page.getByTestId("form-back-3")).toBeDisabled;
-    expect(page.getByTestId("form-back-4")).toBeDisabled;
-    expect(page.getByTestId("form-submit")).toBeDisabled;
-    expect(page.getByTestId("form-model-select")).toBeDisabled;
-    expect(page.getByTestId("form-context-textarea")).toBeDisabled;
-    expect(page.getByTestId("form-prompt-textarea")).toBeDisabled;
-    expect(page.getByTestId("form-temperature-slider")).toBeDisabled;
-  });
-
-  test("correctly checks step 1 input", async ({ page }) => {
-    await page.getByTestId("form-secret-input").fill("s");
-    await page.getByTestId("form-next-1").click();
-    await expect(
-      page.getByText("Key must be at least 10 characters long")
-    ).toBeVisible();
-  });
-
-  test("correctly checks step 3 input", async ({ page }) => {
-    await page.getByTestId("form-secret-input").fill("dddddddddddddddddd");
-    await page.getByTestId("form-next-1").click();
-    await page.getByTestId("form-next-2").click();
-    await page.getByTestId("form-context-textarea").fill("s");
-    await expect(
-      page.getByText("Context must be at least 10 characters long")
-    ).toBeVisible();
-  });
-
-  test("correctly checks step 4 input", async ({ page }) => {
     await page.getByTestId("form-secret-input").fill("sk-12345678910");
     await page.getByTestId("form-next-1").click();
     await page.getByTestId("form-next-2").click();
     await page.getByTestId("form-next-3").click();
-    await page.getByTestId("form-prompt-textarea").fill("");
-    await page.getByTestId("form-submit").click();
-    await expect(
-      page.getByText("Prompt must be at least 10 characters long")
-    ).toBeVisible();
+    await page.getByTestId("form-prompt-textarea").fill("say this is a test");
   });
 
   test("correctly renders a good call", async ({ page }) => {
@@ -126,11 +49,7 @@ test.describe("App", () => {
         await route.fulfill({ json });
       }
     );
-    await page.getByTestId("form-secret-input").fill("sk-12345678910");
-    await page.getByTestId("form-next-1").click();
-    await page.getByTestId("form-next-2").click();
-    await page.getByTestId("form-next-3").click();
-    await page.getByTestId("form-prompt-textarea").fill("say this is a test");
+
     await page.getByTestId("form-submit").click();
     await expect(page.getByText("say this is a test")).toBeVisible();
     await expect(page.getByText("Typing...")).toBeVisible();
@@ -145,11 +64,6 @@ test.describe("App", () => {
         await route.abort("accessdenied");
       }
     );
-    await page.getByTestId("form-secret-input").fill("dddddddddddddddddd");
-    await page.getByTestId("form-next-1").click();
-    await page.getByTestId("form-next-2").click();
-    await page.getByTestId("form-next-3").click();
-    await page.getByTestId("form-prompt-textarea").fill("say this is a test");
     await page.getByTestId("form-submit").click();
     await expect(page.getByText("say this is a test")).toBeVisible();
     await expect(page.getByText("Typing...")).toBeVisible();
@@ -161,10 +75,6 @@ test.describe("App", () => {
   });
 
   test("handles different temperature values", async ({ page }) => {
-    await page.getByTestId("form-secret-input").fill("sk-12345678910");
-    await page.getByTestId("form-next-1").click();
-    await page.getByTestId("form-next-2").click();
-    await page.getByTestId("form-next-3").click();
     const slider = page.getByTestId("form-temperature-slider");
     await slider.click();
     const temperatureValue = page.getByTestId("form-temperature-value");
@@ -223,15 +133,12 @@ test.describe("App", () => {
         await route.fulfill({ json });
       }
     );
-    await page.getByTestId("form-secret-input").fill("sk-12345678910");
-    await page.getByTestId("form-next-1").click();
-    await page.getByTestId("form-next-2").click();
-    await page.getByTestId("form-next-3").click();
-    await page.getByTestId("form-prompt-textarea").fill("say this is a test");
     expect(
       await page.getByTestId("form-temperature-value").innerHTML()
     ).toEqual("0.70");
     await page.getByTestId("form-submit").click();
     await expect(page.getByText("Temperature 0.7")).toBeVisible();
   });
+
+  // TODO: context
 });
---
+++ tests/i18n.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("i18n", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173/react-chatgpt");
+    await page.getByTestId("lang-switch").click();
+  });
+
+  test("works for Russian", async ({ page }) => {
+    await page.getByRole("menuitem", { name: "Русский" }).click();
+    expect(page.getByRole("heading", { name: "Шаг 1: Введите Ключ" }))
+      .toBeVisible;
+  });
+
+  test("works for English", async ({ page }) => {
+    await page.getByRole("menuitem", { name: "English" }).click();
+    await expect(
+      page.getByRole("heading", { name: "Step 1: Provide a Secret" })
+    ).toBeVisible;
+  });
+});
