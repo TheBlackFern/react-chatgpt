29b4dced50e1afbc64e6133880aaa785ccba032a
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2024-02-12 16:38:43 +0300

    🔥 commented out all vitest related tests

--- src/components/form/__tests__/gpt-form.test.tsx
+++ src/components/form/__tests__/gpt-form.test.tsx
@@ -1,54 +1,46 @@
-import { describe, expect, test, vi } from "vitest";
-import { act, screen, getByTestId, render } from "@testing-library/react";
-import renderWithi18next from "@/lib/renderWithI18Next";
-import GPTForm from "../gpt-form";
-import type { TPrompt } from "@/@types";
-import enForm from "@/locales/en/form.json";
-import ruForm from "@/locales/ru/form.json";
+// import { describe, expect, test, vi } from "vitest";
+// import { act, screen, getByTestId, render } from "@testing-library/react";
+// import renderWithi18next from "@/lib/renderWithI18Next";
+// import GPTForm from "../gpt-form";
+// import type { TPrompt } from "@/@types";
 
-let messages: string[] = [];
-// let query = {
-//   secret: "",
-//   model: "gpt-4" as TModel,
-//   prompt: "",
-//   temperature: 0.7,
-// };
+// let messages: string[] = [];
 
-const mockReset = vi.fn();
+// const mockReset = vi.fn();
 
-const mockAddMessage = vi.fn().mockImplementation((text: string) => {
-  messages.push(text);
-});
-const mockMakeQuery = vi.fn().mockImplementation((prompt: TPrompt) => {
-  console.log(JSON.stringify(prompt));
-});
+// const mockAddMessage = vi.fn().mockImplementation((text: string) => {
+//   messages.push(text);
+// });
+// const mockMakeQuery = vi.fn().mockImplementation((prompt: TPrompt) => {
+//   console.log(JSON.stringify(prompt));
+// });
 
-describe("Form", () => {
-  test("should show the first step", async () => {
-    await act(() =>
-      renderWithi18next(
-        <GPTForm
-          makeQuery={mockMakeQuery}
-          addMessage={mockAddMessage}
-          reset={mockReset}
-        />
-      )
-    );
-    expect(await screen.findByTestId("form-step1-heading")).toBeTruthy();
-    screen.debug();
-    // expect(screen.getByText(enForm.step1.heading)).toBeVisible();
-  });
+// describe("Form", () => {
+//   test("should show the first step", async () => {
+//     await act(() =>
+//       renderWithi18next(
+//         <GPTForm
+//           makeQuery={mockMakeQuery}
+//           addMessage={mockAddMessage}
+//           reset={mockReset}
+//         />
+//       )
+//     );
+//     expect(await screen.findByTestId("form-step1-heading")).toBeTruthy();
+//     screen.debug();
+//     // expect(screen.getByText(enForm.step1.heading)).toBeVisible();
+//   });
 
-  test("should not the second and third step", async () => {
-    await act(() =>
-      renderWithi18next(
-        <GPTForm
-          makeQuery={mockMakeQuery}
-          addMessage={mockAddMessage}
-          reset={mockReset}
-        />
-      )
-    );
-    expect(screen.findByTestId("form-step2-heading")).not.toBeTruthy();
-  });
-});
+//   test("should not the second and third step", async () => {
+//     await act(() =>
+//       renderWithi18next(
+//         <GPTForm
+//           makeQuery={mockMakeQuery}
+//           addMessage={mockAddMessage}
+//           reset={mockReset}
+//         />
+//       )
+//     );
+//     expect(screen.findByTestId("form-step2-heading")).not.toBeTruthy();
+//   });
+// });
